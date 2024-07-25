4a60061b60e2ac48806396f1420acf8749255f0b
jagdish <pkjagdish4501@gmail.com>
2024-07-25 19:59:21 +0530

    all functionality added and ready for deployment:

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -10,16 +10,16 @@ interface Player {
 }
 
 const initialData: Player[] = [
-  { userID: "u-1", displayName: "Jone", picture: "", score: 100 },
-  { userID: "u-2", displayName: "Victoria", picture: "", score: 100 },
-  { userID: "u-3", displayName: "Joy", picture: "", score: 100 },
-  { userID: "u-4", displayName: "Quinn", picture: "", score: 100 },
-  { userID: "u-5", displayName: "Sheenalo", picture: "", score: 100 },
-  { userID: "u-6", displayName: "Charlene", picture: "", score: 100 },
-  { userID: "u-7", displayName: "LeonaBaby", picture: "", score: 100 },
-  { userID: "u-8", displayName: "Sunny", picture: "", score: 100 },
-  { userID: "u-9", displayName: "ImWord", picture: "", score: 100 },
-  { userID: "u-10", displayName: "Dophine", picture: "", score: 100 }
+  { userID: "u-1", displayName: "Jone", picture: "", score: 0 },
+  { userID: "u-2", displayName: "Victoria", picture: "", score: 0 },
+  { userID: "u-3", displayName: "Joy", picture: "", score: 0 },
+  { userID: "u-4", displayName: "Quinn", picture: "", score: 0 },
+  { userID: "u-5", displayName: "Sheenalo", picture: "", score: 0 },
+  { userID: "u-6", displayName: "Charlene", picture: "", score: 0 },
+  { userID: "u-7", displayName: "LeonaBaby", picture: "", score: 0 },
+  { userID: "u-8", displayName: "Sunny", picture: "", score: 0 },
+  { userID: "u-9", displayName: "ImWord", picture: "", score: 0 },
+  { userID: "u-10", displayName: "Dophine", picture: "", score: 0 }
 ];
 
 const Leaderboard: React.FC = () => {
